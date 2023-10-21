809f05a5643b29f1196066a2b8f00dfa1146c86d
Samuel Colon <116681627+samcolon@users.noreply.github.com>
2023-10-21 18:14:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   region  = "us-east-1"
-  profile = "leveluptech"
+  profile = "enterprofilenamehere"
 }
 
 # Retrieve the list of AZs in the current AWS region
